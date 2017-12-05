315395d1b7798c6002f519c732d1fddde0bed652
Tonyango <onyangodot@gmail.com>
2017-12-05 11:09:03 +0300

    Login and Registration Pages

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,18 @@
 |
 */
 
+//Authentication Routes
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+//Registration Routes
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
+
+
+
+
 Route::get('blog/{slug}', ['as' => 'blog.single', 'uses' => 'BlogController@getSingle'])->where('slug', '[\w\d\-\_]+');
 
 Route::get('blog', ['uses' => 'BlogController@getArchive', 'as' => 'blog.archive']);
